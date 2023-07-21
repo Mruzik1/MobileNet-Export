1fad85c63a108cadd776907c93185e53d815f4a8
Mruzik1 <mimisha2014@gmail.com>
2023-07-21 19:14:13 +0200

    exported the model without nms

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import sys
 sys.path.append('./mobilenets-ssd-pytorch')
 
+import torch
+import onnx
+import onnxsim
 from vision.ssd.mobilenetv1_ssd_lite import *
 
 
+model = create_mobilenetv1_ssd_lite(80)
+dummy_input = torch.randn((1, 3, 640, 640))
+f_path = 'exported/mobilenet_no_nms.onnx'
+
+torch.onnx.export(model, dummy_input, f_path)
+
+onnx_model = onnx.load(f_path)
+onnx_model, check = onnxsim.simplify(onnx_model)
+onnx.save(onnx_model, f_path)

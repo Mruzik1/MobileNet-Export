1a3a31033b5e8eec023d2dd0a71333246e293d79
Mruzik1 <mimisha2014@gmail.com>
2023-07-26 21:39:21 +0200

    updated exporter

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from custom_models import *
 
 model = create_mobilenetv1_ssd_lite(80, is_test=True)
 dummy_input = torch.randn((1, 3, 300, 300))
-f_path = 'exported/mobilenet.onnx'
+f_path = 'exported/mobilenet_nms.onnx'
 
-print('Exporting...')
-torch.onnx.export(model, dummy_input, f_path)
-print('Exported!')
+# print('Exporting...')
+# torch.onnx.export(model, dummy_input, f_path)
+# print('Exported!')
 
-print('Simplifying...')
-onnx_model = onnx.load(f_path)
-onnx_model, check = onnxsim.simplify(onnx_model)
-onnx.save(onnx_model, f_path)
-print('Simplified!')
+# print('Simplifying...')
+# onnx_model = onnx.load(f_path)
+# onnx_model, check = onnxsim.simplify(onnx_model)
+# onnx.save(onnx_model, f_path)
+# print('Simplified!')
